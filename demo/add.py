64b823125f88e062c04bb807060709894fa796eb
Brian <codingbrian58@gmail.com>
2022-02-22 11:57:02 +0300

    Revert "Adding TestCase for Addition method"

--- demo/add.py
+++ demo/add.py
@@ -1,2 +1,2 @@
 def add(num_1, num_2):
-    return int(num_1) + int(num_2)
+    return num_1 + num_2
--- tests/test_add.py
+++ tests/test_add.py
@@ -5,7 +5,3 @@ class TestAdd(TestCase):
     def test_addition(self):
         result = add(2, 3)
         self.assertEqual(result, 5)
-    
-    def test_addition_of_strings(self):
-        result = add('2', '3')
-        self.assertEqual(result, 5)
